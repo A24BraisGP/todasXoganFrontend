883a08629dc09ccf394a9dd49501c1a635d88ca0
a24braisgp <a24braisgp@iessanclemente.net>
2025-06-16 19:57:05 +0200

    br footer

--- src/components/xeral/components/Footer.tsx
+++ src/components/xeral/components/Footer.tsx
@@ -3,11 +3,8 @@ const Footer = () => {
 		<footer className="footer footer-center p-4 bg-base-200 text-base-content">
 			<div>
 				<p>
-					Copyright © 2025 - Todos os dereitos reservados por
-					TodasXogan
-					<a href="mailto:todasxogan@email.com">
-						todasxogan@email.com
-					</a>
+					Copyright © 2025 - Todos os dereitos reservados por TodasXogan <br />
+					<a href="mailto:todasxogan@email.com">todasxogan@email.com</a>
 				</p>
 			</div>
 		</footer>
